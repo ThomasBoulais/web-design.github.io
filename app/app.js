f8a3e1845213c9c67bc670998468801861e4b1ab
tboulais <thomas.tuan.boulais@gmail.com>
2017-02-24 15:31:06 +0100

    ajout bdd in app.js

--- app/app.js
+++ app/app.js
@@ -41,6 +41,57 @@
             author: "zoe@thomas.com"
           }
         ]
+      },
+      {
+        name: "The Godfather",
+        yearRelease: 1972,
+        director: "Francis Ford Coppola",
+        rating: 9.2,
+        mature: true,
+        plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        thumb: "https://images-na.ssl-images-amazon.com/images/M/MV5BZTRmNjQ1ZDYtNDgzMy00OGE0LWE4N2YtNTkzNWQ5ZDhlNGJmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,704,1000_AL_.jpg",
+        trailer: "https://www.youtube.com/watch?v=sY1S34973zA",
+        reviews: [
+          {
+            stars: 4,
+            body: "He made me an offer i couldn't resist!!",
+            author: "zaga@more.de"
+          }
+        ]
+      },
+      {
+        name: "The Godfather: Part II",
+        yearRelease: 1974,
+        director: "Francis Ford Coppola",
+        rating: 9.0,
+        mature: true,
+        plot: "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+        thumb: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,702,1000_AL_.jpg",
+        trailer: "https://www.youtube.com/watch?v=9O1Iy9od7-A",
+        reviews: [
+          {
+            stars: 2,
+            body: "Plutôt décevant, comme toutes les suites en général en fait.. :/",
+            author: "barbara@boug.ri"
+          }
+        ]
+      },
+      {
+        name: "The Dark Knight",
+        yearRelease: 2008,
+        director: "Christopher Nolan",
+        rating: 9.0,
+        mature: true,
+        plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        thumb: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+        trailer: "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+        reviews: [
+          {
+            stars: 5,
+            body: "My fav movie of all time. A true masterpiece!",
+            author: "thomas.wayne@rip.de"
+          }
+        ]
       }
     ];
   });
